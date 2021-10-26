0acf6176461e0bf67eebd45cbc8314fba5344bb8
javaboy-github <75339009+javaboy-github@users.noreply.github.com>
2021-10-26 22:46:44 +0900

    init commit

---
+++ index.js
@@ -0,0 +1,35 @@
+const Discord = require('discord.js');
+const admin = require("firebase-admin");
+
+var serviceAccount = require("./private_key.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: process.env.databseurl
+});
+
+const database = admin.database();
+const ref = database.ref('napoan-server');
+
+// discord
+const client = new Discord.Client({intents: ["GUILDS", "GUILD_MESSAGES"]});
+const token = process.env.token;
+const info = {};
+
+client.once('ready', () => {
+	console.log('Running discord bot');
+});
+
+client.on('messageCreate', message => {
+	if (message.content.startsWith("/setinfo")) {
+		database.ref(message.author.username).set(message.content.substr(9));
+		message.channel.send("Setted");
+	}
+	if (message.content.startsWith("/getinfo")) {
+		database.ref(message.content.substr(9)).once("value", (value) => {
+			message.channel.send(value.val() || "No data");
+		});
+	}
+});
+
+client.login(token);
---
+++ index.js config.js
@@ -0,0 +1,4 @@
+{
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://discord-jikoshokai-434c0-default-rtdb.asia-southeast1.firebasedatabase.app"
+}
